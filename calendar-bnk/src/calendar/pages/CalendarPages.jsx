02f6fdf87727831aa5777af5ce8a9ccb4eecc1f6
ismaelcd <ismaelcd@gmail.com>
2022-10-06 08:49:09 +0200

    Creada view de calendario

--- calendar-bnk/src/calendar/pages/CalendarPages.jsx
+++
@@ -1,5 +0,0 @@
-export const CalendarPages = () => {
-  return (
-    <div>CalendarPages</div>
-  )
-}
--- calendar/src/calendar/pages/CalendarPages.jsx
+++ calendar/src/calendar/pages/CalendarPages.jsx
@@ -1,9 +1,48 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import enUS from 'date-fns/locale/en-US';
+
 import { Navbar } from "../";
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Compleaños de Ismael',
+  notes: 'Hay que comprar la tarta',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Manolo'
+  }
+}]
 
 export const CalendarPages = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
+
     </>
   )
 }
